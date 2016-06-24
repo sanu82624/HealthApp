042ac4810c3d7d380cab1913d67f69e83cc3acba
sanu82624 <sanu82624@gmail.com>
2016-06-24 13:53:22 +0530

    SS: minor changes

--- www/appjs/controllers/userModule/userRegistrationController.js
+++ www/appjs/controllers/userModule/userRegistrationController.js
@@ -1,48 +1,55 @@
+'use strict';
 
 angular.module('cmaManagementApp').controller('userRegistrationController',[
-	'validationPattern', 'messages', 'userBusiness', 'commonUtility',
-	'$rootScope',
-	function(validationPattern, messages, userBusiness, commonUtility,
-	$rootScope){
+    'validationPattern', 'messages', 'userBusiness', 'commonUtility',
+    '$rootScope',
+    function(validationPattern, messages, userBusiness, commonUtility,
+    $rootScope){
 		
-		var vm = this;
-		
-		vm.validName = validationPattern.NAME;
-		vm.validEmail = validationPattern.EMAIL;
-		vm.validPhone = validationPattern.PHONE;
-		vm.emailMsg = messages.VALID_EMAIL;
-		vm.passMsg = messages.VALID_PASS;
-		
-		vm.onSaveClick = function(){
+        var vm = this;
+
+        vm.validName = validationPattern.NAME;
+        vm.validEmail = validationPattern.EMAIL;
+        vm.validPhone = validationPattern.PHONE;
+        vm.emailMsg = messages.VALID_EMAIL;
+        vm.passMsg = messages.VALID_PASS;
+        vm.nameMsg = messages.VALID_NAME;
+        vm.genderMsg = messages.REQ_GENDER;
+        vm.pinMsg = messages.REQ_PIN;
+        vm.phoneMsg = messages.VALID_PHONE;
+
+        vm.onSaveClick = function(frmData){
+            if(!frmData.userRegForm.$valid){
+                return false;
+            }
+            var userInfo = {};
+            userInfo.name = vm.name;
+            userInfo.age = vm.age;
+            userInfo.address = vm.address;
+            userInfo.pinCode = vm.pinCode;
+            userInfo.gender = vm.gender;
+            userInfo.phone = [vm.phone];
+            userInfo.emailId = [vm.email];
+            userInfo.password = vm.pass;
 			
-			var userInfo = {};
-			userInfo.name = vm.name;
-			userInfo.age = vm.age;
-			userInfo.address = vm.address;
-			userInfo.pinCode = vm.pin;
-			userInfo.gender = vm.gender;
-			userInfo.phone = [vm.phone];
-			userInfo.emailId = [vm.email];
-			userInfo.password = vm.pass;
-			
-			userBusiness.registerUser(userInfo).then(function(response){
-				if(response.data.success){
-					window.alert(messages.USER_REG_SUCCESS);
-					console.info(response);
-					$rootScope.IS_SIGN_IN = response.data.success;
-					$rootScope.NAME = response.data.result.name;
-					$rootScope.ID = response.data.result.clientId;
-					commonUtility.redirectTo("groundVendorHome");
-				} else{
-					window.alert(messages.USER_REG_FAIL);
-				}
-			}, function(error){
-				window.alert(messages.USER_REG_FAIL);
-			});
-		};
+            userBusiness.registerUser(userInfo).then(function(response){
+                if(response.data.success){
+                    window.alert(messages.USER_REG_SUCCESS);
+                    console.info(response);
+                    $rootScope.IS_SIGN_IN = response.data.success;
+                    $rootScope.NAME = response.data.result.name;
+                    $rootScope.ID = response.data.result.clientId;
+                    commonUtility.redirectTo("groundVendorHome");
+                } else{
+                    window.alert(messages.USER_REG_FAIL);
+                }
+            }, function(error){
+                window.alert(messages.USER_REG_FAIL);
+            });
+        };
 		
-		vm.onCancelClick = function(){
-			commonUtility.redirectTo("login");
-		};
-	}
+        vm.onCancelClick = function(){
+            commonUtility.redirectTo("login");
+        };
+    }
 ]);
--- www/appjs/services/constant/messages.js
+++ www/appjs/services/constant/messages.js
@@ -3,13 +3,18 @@
 angular.module('cmaManagementApp')
   .constant('messages', {
       
-      CREATE_REQUEST_SUCCESS: "Request created successfully",
-	  CREATE_REQUEST_ERROR: "Request could not be created",
-	  VALID_EMAIL: "Enter valid email!",
-	  VALID_PASS: "Enter valid password!",
-	  VALID_USER_TYPE: "Select valid type!",
-	  USER_REG_SUCCESS: "Registration is successfully done.",
-	  USER_REG_FAIL: "Registration is failed.",
-	  USER_LOGIN_FAIL: "Login failed.",
-	  USER_LOGIN_WRONG: "Wrong credentials."
+    CREATE_REQUEST_SUCCESS: "Request created successfully",
+    CREATE_REQUEST_ERROR: "Request could not be created",
+    VALID_EMAIL: "Enter valid email!",
+    VALID_PASS: "Enter valid password!",
+    VALID_USER_TYPE: "Select valid type!",
+    USER_REG_SUCCESS: "Registration is successfully done.",
+    USER_REG_FAIL: "Registration is failed.",
+    USER_LOGIN_FAIL: "Login failed.",
+    USER_LOGIN_WRONG: "Wrong credentials.",
+    VALID_NAME: "Enter valid name!",
+    REQ_GENDER: "Select gender!",
+    REQ_PIN: "Enter pin code!",
+    VALID_PHONE: "Enter valid phone!",
+    REQ_SERVICE_TYPE: "Select service type!"
   });
