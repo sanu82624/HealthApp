bb7c9fb3b01c7d46e0231fb7d2907d5a833b56d4
sanu82624 <sanu82624@gmail.com>
2016-07-25 01:55:31 +0530

    SS: Monitor - vendor list & details

--- www/appjs/directives/viewEmail.js
+++ www/appjs/directives/viewEmail.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('cmaManagementApp')
-  .directive('viewPhone', function () {
+  .directive('viewEmail', function () {
     return {
         restricted: "E",
 	replace: true,
@@ -15,11 +15,7 @@ angular.module('cmaManagementApp')
                             '<label class="view-details-item-label">{{label}}: </label>' +
                             '<span class="view-details-item-span">' +
                                 '<span ng-repeat="item in items" class="list-view-items">' +
-                                    '{{item | hideChar:"|"}}&nbsp;' +
-                                    '<a href="tel:{{item | hideChar:\'|\'}}">' +
-                                        '<img class="view-details-item-img" alt="" ' + 
-                                            'src="images/call.png"/>' +
-                                    '</a>' +
+                                    '{{item}}&nbsp;' +
                                 '</span>' +
                             '</span>' +
                         '</div>';
--- www/appjs/directives/viewLocs.js
+++ www/appjs/directives/viewLocs.js
@@ -1,29 +1,33 @@
 'use strict';
 
 angular.module('cmaManagementApp')
-  .directive('viewPhone', function () {
+  .directive('viewLocs', function () {
     return {
         restricted: "E",
 	replace: true,
         scope: {
             label: "@",
-            items: "="
+            items: "=",
+            locClick: "&"
         },
         template: function(){
             
             var html =  '<div class="view-details-item-div">' +
                             '<label class="view-details-item-label">{{label}}: </label>' +
                             '<span class="view-details-item-span">' +
-                                '<span ng-repeat="item in items" class="list-view-items">' +
-                                    '{{item | hideChar:"|"}}&nbsp;' +
-                                    '<a href="tel:{{item | hideChar:\'|\'}}">' +
-                                        '<img class="view-details-item-img" alt="" ' + 
-                                            'src="images/call.png"/>' +
-                                    '</a>' +
+                                '<span ng-repeat="loc in items" class="list-view-items">' +
+                                    '{{loc.name}}&nbsp;' +
+                                    '<img class="view-details-item-img" alt="" ' + 
+                                        'src="images/go.png" ng-click="onLocClick(loc.vendId)" />' +
                                 '</span>' +
                             '</span>' +
                         '</div>';
             return html;
+        },
+        link: function(scope){
+            scope.onLocClick = function(locId){
+                scope.locClick({id: locId});
+            };
         }
     };
   });
